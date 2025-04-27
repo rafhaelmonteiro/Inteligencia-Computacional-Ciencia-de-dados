ec9aa3cd155eb3a3bef938e2ee0f5444d02fcd95
rafhaelmonteiro <rafhaelsmonteiro@gmail.com>
2025-04-27 20:06:02 -0300

    create atv-A and folder assets

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Fundamentos de Inteligência Computacional e Ciencia de dados
 
 Atividade 1 - futuro trabalho com IA
+Apresentaçao: https://docs.google.com/presentation/d/1dPAGBrKT54QU6s0vLXKaPgyuKCEP_jHN-f0A3KMldLA/edit?slide=id.p1#slide=id.p1
 
 Atividade 2 - Z-score e Binomial Distribution
+Colab: 
 
 Atividade 3 - Sample distribution and sampling distribution, TLC and p-value
---
+++ atividades/atividade3/atv-A.py
@@ -0,0 +1,7 @@
+# sample distribution and sampling distribution
+
+# A partir do que foi feito na Atividade 2 
+# “Considerando o primeiro trabalho, defina um experimento em que dados são coletados de uma variável de interesse do seu trabalho”, 
+# crie  uma população artificial dessa variável com uma distribuição Gaussiana e demonstre 
+# sample distribution and sampling distribution. Faça avaliações de probabilidade de possíveis eventos.
+
